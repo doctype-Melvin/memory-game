e27dfaf87aac3b37e2513c739c38e62f6aec4335
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-27 13:46:14 +0100

    puts assets folder at root

--- src/cardsData.jsx
+++ src/cardsData.jsx
@@ -1,32 +1,32 @@
 const data = [
     {
       name: 'diamond',
-      src: '../dist/assets/diamond.png',
+      src: '../assets/diamond.png',
       clicked: false
     },
     {
       name: 'emerald',
-      src: '../dist/assets/emerald.png',
+      src: '../assets/emerald.png',
       clicked: false
     },
     {
       name: 'gold',
-      src: '../dist/assets/gold.png',
+      src: '../assets/gold.png',
       clicked: false
     },
     {
       name: 'chicken',
-      src: '../dist/assets/grass.png',
+      src: '../assets/grass.png',
       clicked: false
     },
     {
       name: 'health',
-      src: '../dist/assets/health.png',
+      src: '../assets/health.png',
       clicked: false
     },
     {
       name: 'pickaxe',
-      src: '../dist/assets/pickaxe.png',
+      src: '../assets/pickaxe.png',
       clicked: false
     }
   ]
