69950054babe3da7954741911f0ec73bd48d7dd7
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-27 13:53:20 +0100

    places assets in public

--- src/cardsData.jsx
+++ src/cardsData.jsx
@@ -1,32 +1,32 @@
 const data = [
     {
       name: 'diamond',
-      src: '../assets/diamond.png',
+      src: './assets/diamond.png',
       clicked: false
     },
     {
       name: 'emerald',
-      src: '../assets/emerald.png',
+      src: './assets/emerald.png',
       clicked: false
     },
     {
       name: 'gold',
-      src: '../assets/gold.png',
+      src: './assets/gold.png',
       clicked: false
     },
     {
       name: 'chicken',
-      src: '../assets/grass.png',
+      src: './assets/grass.png',
       clicked: false
     },
     {
       name: 'health',
-      src: '../assets/health.png',
+      src: './assets/health.png',
       clicked: false
     },
     {
       name: 'pickaxe',
-      src: '../assets/pickaxe.png',
+      src: './assets/pickaxe.png',
       clicked: false
     }
   ]
